f1d46ed9d96c4219a603a8f080e7e1a576be51e4
gabrielnvUFRN <gabriel.vieira.084@ufrn.edu.br>
2024-10-17 21:36:08 -0300

    Create MainBanco.java

---
+++ MainBanco.java
@@ -0,0 +1,18 @@
+public class MainBanco {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("João Silva", "001");
+
+        ContaBancaria conta1 = new ContaBancaria(cliente1);
+
+        conta1.mostrarDadosConta();
+
+        conta1.depositar(500.0);  
+        conta1.mostrarDadosConta();
+
+        conta1.sacar(200.0);      
+        conta1.mostrarDadosConta();
+
+        conta1.sacar(400.0);     
+        conta1.mostrarDadosConta();
+    }
+}
